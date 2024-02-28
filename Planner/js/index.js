58d13ace9cb4d27af382739905ebac09d859dde0
sefiSB <balintsch7@gmail.com>
2024-02-28 17:22:05 +0100

    Started to write the preview

--- Planner/js/index.js
+++ Planner/js/index.js
@@ -1,6 +1,7 @@
 /* import data from 'data/tabledata.json'; */
 const timetable = document.querySelector(".timetable")
 const inside = [...timetable.querySelectorAll("#inside")];
+inside.forEach(cell=>cell.addEventListener("mouseover",preview))
 
 
 const placeLesson = document.querySelector("#placeLesson")
@@ -14,16 +15,33 @@ placeLesson.addEventListener("click", function () {
 const newbtn = document.querySelector("#newbtn")
 const inplength = document.querySelector("#inplength")
 inplength.style.display = "none"
+const numinput = inplength.querySelector("input")
+const okbutton = inplength.querySelector("button")
+okbutton.addEventListener("click",previewStart)
 
 newbtn.addEventListener("click", function () {
     newbtn.style.display = "none"
     inplength.style.display = "block"
 })
 
+let previewOn = false
 
-
-
-function preview(e) {
-    console.log(e.target)
+function previewStart(){
+    if(numinput.innerHTML==""){
+        return
+    }
+    else{
+        if(previewOn){
+            previewOn=false
+        }
+        else{
+            previewOn=true
+        }
+    }
 }
-function place(e) { }
+
+function preview(e){
+    if(previewOn){
+        e.target.style.backgroundcolor="#FF0000"
+    }
+}
